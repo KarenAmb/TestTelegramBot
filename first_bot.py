943e3c4936a79a7e7af0b3407b93e4827e8ac472
Ambartsumyan Karen <kar.amb@yandex.ru>
2018-02-18 17:49:18 +0300

    Создали бота в новой папке

---
+++ first_bot.py
@@ -0,0 +1,34 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+import  settings
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+
+def main():
+    updater = Updater(settings.TELEGRAM_API_KEY)  #ключ теперь в отдельном файле
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start', user_welcomming))
+    dp.add_handler(MessageHandler(Filters.text, talking_with_user))
+    updater.start_polling()
+    updater.idle()
+
+#функция обработки комманды /start
+def user_welcomming(bot, update):
+    my_text = "Здарова {}! Я только начал жить, не суди строго =)".format(update.message.chat.first_name)
+    print(my_text)
+    update.message.reply_text(my_text)
+
+#Функция общения с пользователем
+def talking_with_user(bot, update):
+    user_text = update.message.text
+    logging.info(user_text) #Логируем все что пишет юзер
+    update.message.reply_text(user_text+'. Окей, и что?')
+
+if __name__ == "__main__":
+    logging.info('Bot has been started') #Сообщение о старте успешном бота в логе
+    main()
